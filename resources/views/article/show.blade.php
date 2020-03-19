54ffa67f4fe63235fe5aec74aabd7945d73d1182
Tom <tomleesm@gmail.com>
2020-03-19 17:45:50 +0800

    install Markdown-it javascript library

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,3 +1,16 @@
-<h3>{{ $article->title }}</h3>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>{{ $article->title }}</title>
+    </head>
+    <body>
+    <h3>{{ $article->title }}</h3>
 
-{{ $article->content }}
+    <div class="article content">
+    {{ $article->content }}
+    </div>
+    </body>
+    <script src="{{ url('/js/app.js') }}"></script>
+</html>
