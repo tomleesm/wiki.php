4b4a9a42a4c18279e514d3e7be190879b31e0862
Tom <tomleesm@gmail.com>
2020-03-20 15:44:35 +0800

    setup for sindresorhus/github-markdown-css

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -7,11 +7,13 @@
         <link rel="stylesheet" href="{{ url('/css/app.css')}}" type="text/css">
     </head>
     <body>
-    <h3>{{ $article->title }}</h3>
+    <div class="container markdown-body">
+        <h3>{{ $article->title }}</h3>
 
-    <div class="article content">
+        <div class="article content">
 {{-- 如果 $article->content 有縮排，會造成 markdown 轉 html 的第一行變成 <pre> --}}
 {{ $article->content }}
+        </div>
     </div>
     </body>
     <script src="{{ url('/js/app.js') }}"></script>
