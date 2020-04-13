27b90c968f342c6da4810bfea788dda5d690f259
Tom <tomleesm@gmail.com>
2020-04-13 10:53:34 +0800

    HomeController@index just redirect to /

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,10 +8,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        // 如果有記住之前的網址，就用它，否則使用首頁
-        if($request->session()->has('backUrl'))
-            return redirect(session('backUrl'));
-
         return redirect()->route('article.show', ['title' => 'home']);
     }
 }
