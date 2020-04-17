7201ce80924b57b1e0bd898241537d3195deac8e
Tom <tomleesm@gmail.com>
2020-04-17 14:18:05 +0800

    show message when search result is empty

--- resources/views/article/search.blade.php
+++ resources/views/article/search.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.search')
 
 @section('content')
-    @foreach ($query->result as $result)
-    <p>
-        <a href="{{ route('article.show', ['title' => $result['title']]) }}">{{ $result['title'] }}</a>
-    </p>
-    <p>{{ $result['content'] }}</p>
-@endforeach
+    @if($query->result->count() === 0)
+        <p>We can't find anything. Please try other keywords.</p>
+    @else
+        @foreach ($query->result as $result)
+        <p>
+            <a href="{{ route('article.show', ['title' => $result['title']]) }}">{{ $result['title'] }}</a>
+        </p>
+        <p>{{ $result['content'] }}</p>
+        @endforeach
+    @endempty
 @endsection
