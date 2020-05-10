32d791c3d1845cf0452c9cf89986dc986b35dc62
Tom <tomleesm@gmail.com>
2020-05-10 22:04:42 +0800

    新增 dusk 測試：會員登入驗證
    - phpunit.xml 刪除 sqlite:memory: 花的時間一樣多，而且可以實際操作確認爲何測試失敗
    - input type 改成 text，才能顯示 Laravel 提供的錯誤訊息，不是瀏覽器本身的訊息

--- tests/Browser/WikiTest.php
+++ tests/Browser/WikiTest.php
@@ -5,10 +5,19 @@ namespace Tests\Browser;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Artisan;
 
 class WikiTest extends DuskTestCase
 {
-    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:refresh');
+    }
 
     /**
      * 尚未有任何條目時，條目 home 顯示提示訊息
@@ -25,4 +34,60 @@ class WikiTest extends DuskTestCase
         });
     }
 
+    /**
+     * 會員登入認證
+     */
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+
+            // 新增一個使用者
+            $user = new User();
+            $user->name = 'Tom';
+            $user->email = 'tomleesm@gmail.com';
+            $password = '123456';
+            $user->password = Hash::make($password);
+            $user->email_verified_at = now();
+            $user->remember_token = Str::random(10);
+            $user->save();
+
+            $this->assertDatabaseHas('users', [
+                'name' => 'Tom',
+                'email' => 'tomleesm@gmail.com'
+            ]);
+
+            // 連到首頁，點選 Login
+            $browser->visit('/')
+                    ->clickLink('Login')
+                    ->assertPathIs('/input/username')
+                    // 直接點選 Next，會提示要輸入
+                    ->press('Next')
+                    ->assertSee('Please type E-mail.')
+                    // 驗證輸入值爲 E-mail
+                    ->type('email', 'vdlnewonewoew')
+                    ->press('Next')
+                    ->assertSee('Input is not E-mail. Please type again.')
+                    // 驗證已經有這個使用者
+                    ->type('email', 'notUser@email.com')
+                    ->press('Next')
+                    ->assertSee('Not registered E-mail.')
+                    // 輸入正確的 E-mail
+                    ->type('email', $user->email)
+                    ->press('Next')
+                    ->assertPathIs('/input/password')
+                    // 欄位密碼上方顯示 E-mail
+                    ->assertSee($user->email)
+                    // 密碼不能是空的
+                    ->press('Login')
+                    ->assertSee('Please type password.')
+                    // 輸入錯誤的密碼
+                    ->type('password', 'wrong password')
+                    ->press('Login')
+                    ->assertSee('Not correct password. Please try again.')
+                    // 登入成功，跳轉回首頁
+                    ->type('password', $password)
+                    ->press('Login')
+                    ->assertPathIs('/read/home');
+        });
+    }
 }
