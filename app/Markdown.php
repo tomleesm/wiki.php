5e53c056bb03a8b66383fc8f55a7f162097a73fc
Tom <tomleesm@gmail.com>
2020-09-09 00:13:31 +0800

    程式碼加上註解

--- app/Markdown.php
+++ app/Markdown.php
@@ -9,47 +9,51 @@ class Markdown extends \ParsedownToC
     private $breadcrumbParent = '';
 
     public function __construct($markdown, $breadcrumbParent = '') {
-        parent::__construct();
         // 防止 XSS
         $this->setSafeMode(true);
 
         $this->markdown = $markdown;
+        // 上一層麵包屑
         $this->breadcrumbParent = $breadcrumbParent;
     }
 
     public function toHTML() {
+        // 把 markdown 轉換成 html
         $html = $this->text($this->markdown);
+        // 把 [[test]] 轉成連結 /read/test?parent=breadcrumbParent
         return $this->convertWikiLinks($html);
     }
 
     public function toPDF() {
-        // markdown 轉換成 HTML
-        // PDF 不產生目錄，所以是用 ParsedownToC->body()
         // 設定使用中文字型
         // 中文字型屬於自訂字型，不能用 $dompdf->set_option('defaultFont', 'wt011');
         $html = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
         $html .= '<style>@font-face { font-family: "wt011"; } * { font-family: "wt011" }</style>';
+        // markdown 轉換成 HTML。PDF 不產生目錄，所以是用 ParsedownToC->body()
         $html .= $this->body($this->markdown);
 
+        // 刪除 [toc]
         $html = $this->removeToCTag($html);
 
         // HTML 轉換成 PDF
         $dompdf = new Dompdf();
-        //$dompdf->loadHtml($html);
         $dompdf->loadHtml($html);
         // 設定紙張大小和直橫式
         $dompdf->setPaper('A4', 'landscape');
         // 轉換 HTML 爲 PDF
         $dompdf->render();
-        // 下載到 client
+        // 觸發下載 PDF 檔的 HTTP 請求
         $dompdf->stream();
     }
 
+    // 轉換 [[]] 爲 wiki link
     private function convertWikiLinks($html) {
-        // 轉換 [[]] 爲 wiki link
         return preg_replace_callback('/\[\[([^\]]+)\]\]/', function($matches) {
+            // 抓取 [[test]] 之間的文字
             $linkText = $matches[1];
+            // url = /read/test?parent=home
             $URL = sprintf('/read/%s?parent=%s', urlEncode($linkText), urlEncode($this->breadcrumbParent));
+            // 回傳 <a href="/read/test?parent=home">test</a>
             return sprintf('<a href="%s">%s</a>', $URL, $linkText);
         }, $html);
     }
