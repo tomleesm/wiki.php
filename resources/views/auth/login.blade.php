f3472c24ced2ddf4fc43c69b12eae041ad492b98
Tom <tomleesm@gmail.com>
2020-11-02 10:44:49 +0800

    remove google and github logo icon from oauth login buttons
    因爲可能有版權問題

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,12 @@
     <div class="col-12 col-lg-4 border border-secondary rounded">
         <p class="text-center mt-3">
           <a class="btn btn-outline-primary btn-lg btn-block" role="button" href="/login/google">
-            <i class="fab fa-google"></i> Login with Google
+             Login with Google
           </a>
         </p>
         <p class="text-center">
           <a class="btn btn-outline-secondary btn-lg btn-block" role="button" href="/login/github">
-            <i class="fab fa-github"></i> Login with GitHub
+             Login with GitHub
           </a>
         </p>
     </div>
