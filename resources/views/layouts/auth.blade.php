93ca6924deb2f72473d5361e2837b881383586d3
Tom <tomleesm@gmail.com>
2020-11-05 16:30:51 +0800

    remove font-awesome css link

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,9 +7,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Login - {{ env('APP_NAME', 'wiki.php') }}</title>
         <link rel="stylesheet" href="{{ asset('css/app.css')}}" type="text/css">
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css" integrity="sha512-kJ30H6g4NGhWopgdseRb8wTsyllFUYIx3hiUwmGAkgA9B/JbzUBDQVr2VVlWGde6sdBVOG7oU8AL35ORDuMm8g==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/brands.min.css" integrity="sha512-D0B6cFS+efdzUE/4wh5XF5599DtW7Q1bZOjAYGBfC0Lg9WjcrqPXZto020btDyrlDUrfYKsmzFvgf/9AB8J0Jw==" crossorigin="anonymous" />
     </head>
     <body>
     <div class="container">
