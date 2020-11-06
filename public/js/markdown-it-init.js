ec9e59dcd1d600d45e623090801d31c1369f7736
Tom <tomleesm@gmail.com>
2020-11-06 18:31:37 +0800

    remove all css, js, and image files in public/

--- public/js/markdown-it-init.js
+++
@@ -1,7 +0,0 @@
-MarkdownIt = require('markdown-it'),
-  md = new MarkdownIt({
-    html:         false,
-    langPrefix:   'highlight highlight-source-', // 配合 Markdown CSS
-    linkify:      true,
-    typographer:  true,
-  });
