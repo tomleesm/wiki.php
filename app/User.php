73fcf9dcf1a461c754974efd3d08dbd5e38cc39e
Tom <tomleesm@gmail.com>
2020-11-07 18:08:00 +0800

    第一個登入的使用者是 Administrator

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'oauth_id', 'provider', 'role_id'
     ];
 
     /**
