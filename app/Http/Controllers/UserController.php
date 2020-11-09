3978c2a13fd67562750080967d2d22fe6a16b479
Tom <tomleesm@gmail.com>
2020-11-09 14:03:30 +0800

    訪問 route user.auth 時，登入的使用者必須是 administrator

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Role;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function __construct() {
         $this->middleware('auth');
     }
+
     public function auth() {
+        $this->authorize('auth', Auth::user());
+
         $users = User::all();
         $roles = Role::all();
         $onlyOneAdmin = Role::where('name', 'Administrator')->first()->users()->count() === 1;
