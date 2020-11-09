d4eaaf217544bcdca7a4fa82e10231b7d6c4c328
Tom <tomleesm@gmail.com>
2020-11-09 13:53:49 +0800

    先登入才能存取 UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,9 @@ use App\Role;
 
 class UserController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
     public function auth() {
         $users = User::all();
         $roles = Role::all();
