9f48c5e0f0403150971210184d2995c370c4357e
Tom <tomleesm@gmail.com>
2020-11-11 14:44:21 +0800

    切換選單後，顯示確認 modal

---
+++ resources/views/partials/articles/auth-confirm-modal.blade.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div class="modal fade" id="article-auth-confirm-modal" tabindex="-1" role="dialog" aria-labelledby="article-auth-confirm-modal-title" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="article-auth-confirm-modal-title">Change role ?</h5>
+        <button type="button" class="close cancel" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <p></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary yes">Yes</button>
+        <button type="button" class="btn btn-secondary no" data-dismiss="modal">No</button>
+      </div>
+    </div>
+  </div>
+</div>
