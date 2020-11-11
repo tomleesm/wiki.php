f60cd29c1c664d074c9340a55c8bf98776828c20
Tom <tomleesm@gmail.com>
2020-11-12 01:24:14 +0800

    新增 UserTablesSeeder，並修改 UserFactory，以方便測試使用者權限分頁

---
+++ database/seeds/UserTablesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class, 110)->create();
+    }
+}
