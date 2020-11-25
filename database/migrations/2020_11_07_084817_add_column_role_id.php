f163fb9ddcd77e157f7421cb4c9dfaae90795da1
Tom <tomleesm@gmail.com>
2020-11-25 14:00:17 +0800

    set default(1) in migration 2020_11_07_084817_add_column_role_id

--- database/migrations/2020_11_07_084817_add_column_role_id.php
+++ database/migrations/2020_11_07_084817_add_column_role_id.php
@@ -14,7 +14,7 @@ class AddColumnRoleId extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role_id');
+            $table->integer('role_id')->default(1);
 
             $table->foreign('role_id')
                   ->references('id')->on('roles')
