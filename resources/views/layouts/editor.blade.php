8d09caac6c979841765ddf67261f2a25d2b447fc
Tom <tomleesm@gmail.com>
2020-11-30 08:18:42 +0800

    remove unnecessary <div class="container"> in view layouts.editor

--- resources/views/layouts/editor.blade.php
+++ resources/views/layouts/editor.blade.php
@@ -1,57 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    {{-- 上傳進度 --}}
-    <div class="fixed-top">
-        <p class="text-right d-none uploading notification"><img src="/img/loading.svg"> uploading...</p>
+{{-- 上傳進度 --}}
+<div class="fixed-top">
+    <p class="text-right d-none uploading notification"><img src="/img/loading.svg"> uploading...</p>
+</div>
+
+@yield('modal-alert', '')
+
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="edit-tab" data-toggle="tab" href="#edit" role="tab" aria-controls="edit" aria-selected="true">@yield('tab-label')</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="preview-tab" data-toggle="tab" href="#preview" role="tab" aria-controls="preview" aria-selected="false">Preview</a>
+  </li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+    {{-- 輸入 div --}}
+    <div class="tab-pane edit active" id="edit" role="tabpanel" aria-labelledby="edit-tab">
+        {{-- 條目標題 --}}
+        <h3>{{ $article->title }}</h3>
+        @yield('form')
+            @csrf
+            @yield('method-extra', '')
+
+            @yield('hidden-article-id', '')
+
+            {{-- 用隱藏欄位傳送條目標題，在 ArticleController@update 尋找 model Article --}}
+            @include('layouts.hiddenArticleTitle', ['article' => $article])
+            {{-- 工具列用來觸發檔案選擇的 <input type="file"> --}}
+            @include('layouts.fileDialog')
+
+            {{-- 如果 textarea 有縮排，會造成 markdown 轉 html 的第一行變成 <pre> --}}
+            <textarea name="article[content]" id="editArticleContent" class="form-control" draggable="true">@yield('textarea-old-value', '')</textarea>
+    </div>
+    {{-- 預覽 div --}}
+    <div class="tab-pane" id="preview" role="tabpanel" aria-labelledby="preview-tab">
+        <h3>{{ $article->title }}</h3>
+        <div class="preview markdown-body">
+        </div>
     </div>
 
-    @yield('modal-alert', '')
-
-    <!-- Nav tabs -->
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-      <li class="nav-item">
-        <a class="nav-link active" id="edit-tab" data-toggle="tab" href="#edit" role="tab" aria-controls="edit" aria-selected="true">@yield('tab-label')</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" id="preview-tab" data-toggle="tab" href="#preview" role="tab" aria-controls="preview" aria-selected="false">Preview</a>
-      </li>
-    </ul>
-
-    <!-- Tab panes -->
-    <div class="tab-content">
-        {{-- 輸入 div --}}
-        <div class="tab-pane edit active" id="edit" role="tabpanel" aria-labelledby="edit-tab">
-            {{-- 條目標題 --}}
-            <h3>{{ $article->title }}</h3>
-            @yield('form')
-                @csrf
-                @yield('method-extra', '')
-
-                @yield('hidden-article-id', '')
-
-                {{-- 用隱藏欄位傳送條目標題，在 ArticleController@update 尋找 model Article --}}
-                @include('layouts.hiddenArticleTitle', ['article' => $article])
-                {{-- 工具列用來觸發檔案選擇的 <input type="file"> --}}
-                @include('layouts.fileDialog')
-
-                {{-- 如果 textarea 有縮排，會造成 markdown 轉 html 的第一行變成 <pre> --}}
-                <textarea name="article[content]" id="editArticleContent" class="form-control" draggable="true">@yield('textarea-old-value', '')</textarea>
-        </div>
-        {{-- 預覽 div --}}
-        <div class="tab-pane" id="preview" role="tabpanel" aria-labelledby="preview-tab">
-            <h3>{{ $article->title }}</h3>
-            <div class="preview markdown-body">
-            </div>
-        </div>
-
-            @section('btn-save-cancel')
-            <button class="btn btn-primary">Save</button>
-            <a class="btn" href="{{ route('articles.show', ['title' => $article->title]) }}">Cancel</a>
-            @show
-        </form>
-    </div>
+        @section('btn-save-cancel')
+        <button class="btn btn-primary">Save</button>
+        <a class="btn" href="{{ route('articles.show', ['title' => $article->title]) }}">Cancel</a>
+        @show
+    </form>
 </div>
 @endsection
 
